9c319d9aa4caf2712c697ead159d31ecced0a88b
sehun <seihun2z@gmail.com>
2023-02-13 22:18:38 +0900

    update main file

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rotate_math = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rotate_math, image.shape[1::-1], flags = cv2.INTER_LINEAR, borderValue = (255, 255, 255))
+    return result
 
 cap = cv2.VideoCapture(0)
 
@@ -24,7 +29,14 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
             for detection in results.detections:
                 mp_drawing.draw_detection(image, detection)
 
-        cv2.imshow('MediaPipe Face Detection', cv2.resize(image, None, fx=0.5, fy=0.5))
+                tan_theta = (left_eye[1] - right_eye[1]) / (right_eye[0] - left_eye[0])
+                theta = np.arctan(tan_theta)
+                rotate_angle = theta * 180 / math.pi
+                rotate_image_right_eye = rotate_image(image_right_eye, rotate_angle)
+                rotate_image_left_eye = rotate_image(image_left_eye, rotate_angle)
+                rotate_image_nose = rotate_image(image_nose, rotate_angle)
+
+        cv2.imshow('Face detection', cv2.resize(image, None, fx=0.5, fy=0.5))
 
         if cv2.waitKey(1) == ord('q'):
             break
